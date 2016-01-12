47b44fe56c3add21e96c3f09cc316360dbfed0f4
Adrien Vergé <adrienverge@gmail.com>
2016-01-12 17:33:09 +0100

    Distribution: Add a setup.py script

---
+++ setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016 Adrien Vergé
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+from yamllint import (__author__, __license__,
+                      APP_NAME, APP_VERSION, APP_DESCRIPTION)
+
+
+setup(
+    name=APP_NAME,
+    version=APP_VERSION,
+    author=__author__,
+    description=APP_DESCRIPTION,
+    license=__license__,
+    keywords=['yaml', 'lint', 'linter'],
+    url='https://github.com/adrienverge/yamllint',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+    ],
+
+    packages=find_packages(),
+    scripts=['bin/yamllint'],
+    package_data={'yamllint': ['conf/*.yml']},
+    install_requires=[
+        'pyyaml>=3'
+    ],
+)
