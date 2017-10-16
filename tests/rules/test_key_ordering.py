773bfc0f3c6ba3179ced739b9e840478acddb4b7
Adrien Vergé <adrienverge@gmail.com>
2017-10-16 22:43:36 +0200

    key-ordering: Add more test cases and documentation

--- tests/rules/test_key_ordering.py
+++ tests/rules/test_key_ordering.py
@@ -59,6 +59,55 @@ class KeyOrderingTestCase(RuleTestCase):
                    '  third:\n'
                    '    second: 2\n', conf)
 
+    def test_word_length(self):
+        conf = 'key-ordering: enable'
+        self.check('---\n'
+                   'a: 1\n'
+                   'ab: 1\n'
+                   'abc: 1\n', conf)
+        self.check('---\n'
+                   'a: 1\n'
+                   'abc: 1\n'
+                   'ab: 1\n', conf,
+                   problem=(4, 1))
+
+    def test_key_duplicates(self):
+        conf = ('key-duplicates: disable\n'
+                'key-ordering: enable')
+        self.check('---\n'
+                   'key: 1\n'
+                   'key: 2\n', conf)
+
+    def test_case(self):
+        conf = 'key-ordering: enable'
+        self.check('---\n'
+                   'T-shirt: 1\n'
+                   'T-shirts: 2\n'
+                   't-shirt: 3\n'
+                   't-shirts: 4\n', conf)
+        self.check('---\n'
+                   'T-shirt: 1\n'
+                   't-shirt: 2\n'
+                   'T-shirts: 3\n'
+                   't-shirts: 4\n', conf,
+                   problem=(4, 1))
+
+    def test_accents(self):
+        conf = 'key-ordering: enable'
+        self.check('---\n'
+                   'hair: true\n'
+                   'hais: true\n'
+                   'haïr: true\n'
+                   'haïssable: true\n', conf)
+        self.check('---\n'
+                   'haïr: true\n'
+                   'hais: true\n', conf,
+                   problem=(3, 1))
+        self.check('---\n'
+                   'haïr: true\n'
+                   'hais: true\n', conf,
+                   problem=(3, 1))
+
     def test_key_tokens_in_flow_sequences(self):
         conf = 'key-ordering: enable'
         self.check('---\n'
