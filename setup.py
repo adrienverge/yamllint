c829254b3d96a252ead468df4a3db075d7cd6151
Satoru SATOH <satoru.satoh@gmail.com>
2020-09-01 23:51:25 +0900

    Move setuptools' packaging configuration from setup.py to setup.cfg
    Move setuptools' packaging configuration from setup.py to setup.cfg to
    simplify setup.py and make its packaging more dedeclarative.
    
    Signed-off-by: Satoru SATOH <satoru.satoh@gmail.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 from yamllint import (__author__, __license__,
                       APP_NAME, APP_VERSION, APP_DESCRIPTION)
@@ -27,31 +27,4 @@ setup(
     description=APP_DESCRIPTION.split('\n')[0],
     long_description=APP_DESCRIPTION,
     license=__license__,
-    keywords=['yaml', 'lint', 'linter', 'syntax', 'checker'],
-    url='https://github.com/adrienverge/yamllint',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Debuggers',
-        'Topic :: Software Development :: Quality Assurance',
-        'Topic :: Software Development :: Testing',
-    ],
-
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    entry_points={'console_scripts': ['yamllint=yamllint.cli:run']},
-    package_data={'yamllint': ['conf/*.yaml']},
-    install_requires=['pathspec >=0.5.3', 'pyyaml'],
-    test_suite='tests',
 )
