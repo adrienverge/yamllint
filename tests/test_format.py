32de4472052b28f45b1197c07d43cfe117dc9069
QuentinN42 <quentin@lieumont.fr>
2022-02-10 12:20:42 +0100

    test: severity_from_level

--- tests/test_format.py
+++ tests/test_format.py
@@ -22,3 +22,19 @@ class TextToXMLTestCase(unittest.TestCase):
     def test_specials_chars(self):
         for inp, out in self.specials.items():
             self.assertEqual(escape_xml(inp), out)
+
+
+class CodeClimateSeverityTestCase(unittest.TestCase):
+
+    expected = {
+        None: "info",
+        'warning': "minor",
+        'error': "major",
+        0: "info",
+        1: "minor",
+        2: "major",
+    }
+
+    def test_specials_chars(self):
+        for inp, out in self.expected.items():
+            self.assertEqual(severity_from_level(inp), out)
