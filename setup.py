bb8300f70585d988dae90f8d4f2d32fc7507d52c
Adrien Vergé <adrienverge@gmail.com>
2023-04-14 16:40:01 +0200

    build: Migrate from setup.py to pyproject.toml
    Using `setup.py` is deprecated and the new recommanded way is to declare
    a `pyproject.toml` file (see PEP 517 [^1]).
    
    This commit proposes to use setuptools to achieve that, because
    setuptools is already used by yamllint, is standard and referenced by
    the official Python packaging documentation [^2], and seems to be the
    most lightweight solution. An alternative could have been to use Poetry,
    see the dedicated pull request and discussion [^3].
    
    For some period, the `setup.py` file will be kept (although almost
    empty), to allow old tools versions to keep working.
    
    Closes https://github.com/adrienverge/yamllint/issues/509.
    
    [^1]: https://peps.python.org/pep-0517/
    [^2]: https://packaging.python.org/en/latest/tutorials/installing-packages/
    [^3]: https://github.com/adrienverge/yamllint/pull/557

--- setup.py
+++ setup.py
@@ -15,15 +15,6 @@
 
 from setuptools import setup
 
-from yamllint import (__author__, __license__,
-                      APP_NAME, APP_VERSION, APP_DESCRIPTION)
-
-
-setup(
-    name=APP_NAME,
-    version=APP_VERSION,
-    author=__author__,
-    description=APP_DESCRIPTION.split('\n')[0],
-    long_description=APP_DESCRIPTION,
-    license=__license__,
-)
+# This is only kept for backward-compatibility with older versions that don't
+# support new packaging standards (e.g. PEP 517 or PEP 660):
+setup()
