36da492ad4c49d624902159caa4506a57060e174
Avihai Shalom <avihais12344@gmail.com>
2026-06-11 18:17:52 +0300

    Forgot to add sef.

--- yamllint/formats/project_format.py
+++ yamllint/formats/project_format.py
@@ -2,9 +2,12 @@ from abc import ABC, abstractmethod
 from collections.abc import Iterable
 from pathlib import Path
 
+from typing_extensions import Self
+
 from yamllint.linter import LintProblem
+
 
 class ProjectFormat(ABC):
     @abstractmethod
-    def format(workdir: Path, problems: Iterable[LintProblem]) -> str:
+    def format(self: Self, workdir: Path, problems: Iterable[LintProblem]) -> str:
         pass
