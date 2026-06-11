0447694ead78520a9cc215d7f3de1a6a4600652d
Avihai Shalom <avihais12344@gmail.com>
2026-06-11 18:38:06 +0300

    Added my idea of lint problem.

---
+++ yamllint/lint_problem.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import Annotated
+
+from attrs import define, field
+from annotated_types import Ge, MinLen
+
+
+@define(frozen=True, eq=True, order=True,)
+class LintProblem:
+    file: Path
+    line: Annotated[int, Ge(0)]
+    column: Annotated[int, Ge(0)]
+    desc: str = field(eq=False, order=False,)
+    rule: Annotated[str, MinLen(1)] = field(order=False,)
+    # Don't compare the level as if they
+    # point to the same location and talk about
+    # the same thing, that's enough.
+    level: None | str = field(eq=False, order=False,)
